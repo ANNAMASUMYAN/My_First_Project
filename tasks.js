a9d37828bafeef541f3193f8b421d205778c6753
ANNA MASUMYAN <a.masumyan1985@gmail.com>
2022-01-11 20:44:21 +0400

    first commit

---
+++ tasks.js
@@ -0,0 +1,25 @@
+// let admin, name;
+// name = "John";
+// admin = name;
+// alert(admin)
+
+let i = 11;
+let n = i++%5;
+alert(i);
+alert(n);
+
+
+// let a = 10, b = 3, c;
+// c = a;
+// a = b;
+// b = c;
+// alert(`a = ${a}`);
+// alert (`b = ${b}`);
+
+
+// let a = 10, b = 3;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// alert(`a = ${a}`);
+// alert (`b = ${b}`);
